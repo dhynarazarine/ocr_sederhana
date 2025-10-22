5169625c37232184f76fac64cc258cd5037a6b19
dhynarazarine <dinar.andiyani@gmail.com>
2025-10-22 17:51:37 +0700

    UTS: Selesai Soal 3 - Implementasi Flutter TTS

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart'; 
+import 'package:flutter_tts/flutter_tts.dart';
+import 'home_screen.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String ocrText;
-
   const ResultScreen({super.key, required this.ocrText});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    // Inisialisasi FlutterTTS
+    flutterTts = FlutterTts();
+    // Atur bahasa ke Bahasa Indonesia
+    flutterTts.setLanguage("id-ID");
+  }
+
+  @override
+  void dispose() {
+    // Hentikan TTS saat halaman ditutup
+    flutterTts.stop();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +36,17 @@ class ResultScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
+          // Menampilkan teks hasil OCR
           child: SelectableText(
-            ocrText.isEmpty ? 'Tidak ada teks ditemukan.' : ocrText,
+            widget.ocrText.isEmpty
+                ? 'Tidak ada teks ditemukan.'
+                : widget.ocrText,
             style: const TextStyle(fontSize: 18),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        // Tombol kembali ke HomeScreen
         onPressed: () {
           Navigator.pushAndRemoveUntil(
             context,
